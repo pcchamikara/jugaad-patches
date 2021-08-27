03bab754723a3d949a27f3097dfffb0f2febe5b5
pcchamikara <pcchamikara@gmail.com>
2021-08-27 13:56:57 +0530

    modified ignore

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -797,7 +797,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
 $databases['default']['default'] = array (
-  'database' => 'dr1',
+  'database' => 'jugaad-db',
   'username' => 'root',
   'password' => '',
   'prefix' => '',
